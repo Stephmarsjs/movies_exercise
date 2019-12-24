cf4327990702275375d814b9736822f1cbede5f5
Stephanie Rodriguez <stephanierodriguez.student@careerdevs.com>
2019-12-24 12:16:31 -0500

    made function async

--- exercise.js
+++ exercise.js
@@ -1,14 +1,28 @@
-getCustomer(1, (customer) => {
+// getCustomer(1, (customer) => {
+//     console.log('Customer: ', customer);
+//     if (customer.isGold) {
+//         getTopMovies((movies) => {
+//             console.log('Top movies: ', movies);
+//             sendEmail(customer.email, movies, () => {
+//                 console.log('Email sent...')
+//             });
+//         });
+//     }
+// });
+
+
+async function notifyCutstomer() {
+    const customer = await getCustomer(1);
     console.log('Customer: ', customer);
     if (customer.isGold) {
-        getTopMovies((movies) => {
-            console.log('Top movies: ', movies);
-            sendEmail(customer.email, movies, () => {
-                console.log('Email sent...')
-            });
-        });
-    }
-});
+      const movies = await getTopMovies();
+      console.log('Top movies: ', movies);
+      await sendEmail(customer.email, movies);
+      console.log('Email sent...')
+ }
+}
+notifyCutstomer();
+
 
 function getCustomer(id) {
     return new Promise((resolve, reject) => {
